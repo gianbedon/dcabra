5b4833de1c58311de57ccfc86832c4042e090d37
ramirols <ramirols.dev@gmail.com>
2026-06-26 11:43:47 -0500

    :tada: actualizando ssr en astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
+
+  adapter: vercel({
+    maxDuration: 10,
+  }),
+
   vite: {
     plugins: [tailwindcss()]
   },
